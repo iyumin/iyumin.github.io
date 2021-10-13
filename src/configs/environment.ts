25932268e83687d43649895fc2e9971bf905175f
kevinjobs <kevinjobs@qq.com>
2021-10-13 20:30:55 +0800

    fix: CNAME

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,14 +1,14 @@
 export const ENV = {
   develop: {
     api: {
-      baseUrl: 'http://localhost:5000/v2',
-      v2BaseUrl: 'http://localhost:5000/v2',
+      baseUrl: 'https://api.kevinjobs.com:15941/v2',
+      v2BaseUrl: 'https://api.kevinjobs.com:15941/v2',
     },
   },
   production: {
     api: {
-      baseUrl: 'https://api.iyum.in:5000/v2',
-      v2BaseUrl: 'https://api.iyum.in:5000/v2',
+      baseUrl: 'https://api.kevinjobs.com:15941/v2',
+      v2BaseUrl: 'https://api.kevinjobs.com:15941/v2',
     }
   },
 };
