27c4f0fac2fdfb13efca9c0c3820e621f3a6abaa
kevinjobs <kevinjobs@qq.com>
2021-12-29 15:33:32 +0800

    update config

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,14 +1,14 @@
 export const ENV = {
   develop: {
     api: {
-      baseUrl: 'https://api.iyum.in:15941/v2',
-      v2BaseUrl: 'https://api.iyum.in:15941/v2',
+      baseUrl: 'http://api.kevinjobs.com:5000/v2',
+      v2BaseUrl: 'http://api.kevinjobs.com:5000/v2',
     },
   },
   production: {
     api: {
-      baseUrl: 'https://api.iyum.in:15941/v2',
-      v2BaseUrl: 'https://api.iyum.in:15941/v2',
+      baseUrl: 'http://api.kevinjobs.com:5000/v2',
+      v2BaseUrl: 'http://api.kevinjobs.com:5000/v2',
     }
   },
 };
