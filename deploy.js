534b2d356f91d7a6dad9a833db3e0842941c6273
Kevin Jobs <me@kevinjobs.com>
2023-10-25 14:10:04 +0800

    feat: delete useless file

--- deploy.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const ghpages = require('gh-pages');
-
-ghpages.publish('dist', {
-  branch: 'gh-pages',
-  repo: 'git@github.com:kevinjobs/kevinjobs.github.io.git'
-}, function(err) {
-  console.log(err);
-});
